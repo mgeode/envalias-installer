4637b6ca70b6af3f3c5b7d2ebd9a26a7890e0ec6
meiching <accounts@mgeo.de>
2019-01-21 22:36:14 +0100

    SetupWizard only

--- src/setup.sh
+++ src/setup.sh
@@ -4,12 +4,20 @@ FILE_EXPORT=src/private.d/user.exports
 LIST_KEYS="USER_A USER_B HOMELAN_IP PROXY_HTTPADD REGISTRY_NPM"
 
 question() {
-    echo "Give me Name of $1:"
+    echo "* Give me Name of '$1':"
     read value
     echo "export ${1}=${value}" >>${FILE_EXPORT}
 }
+
+clear
+echo "***"
+echo "* SETUP Wizard on ${FILE_EXPORT}"
+echo "* return only, if should not set!"
+echo "***"
+echo ""
 
 echo -e "#\n####USER_EXPORTS by setupwizard" >${FILE_EXPORT}
 for svc in $LIST_KEYS; do
     question "${svc}"
 done
+echo "" >${FILE_EXPORT}
